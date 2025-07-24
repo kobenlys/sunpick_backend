04bc5de094193a18d1cb1dcc29c3d11518277275
kobenlys <vlfxhd69@naver.com>
2025-07-24 16:52:57 +0900

    init: 초기 환경설정

---
+++ sunpick/src/main/java/com/backend/sunpick/SunpickApplication.java
@@ -0,0 +1,13 @@
+package com.backend.sunpick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunpickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SunpickApplication.class, args);
+	}
+
+}
---
+++ sunpick/src/test/java/com/backend/sunpick/SunpickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.sunpick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunpickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
